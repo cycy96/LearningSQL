ae413197e288d85af1b8797e7a53b7ee12726ec2
LEONG CHEE YAN <127811480+cycy96@users.noreply.github.com>
2023-05-12 09:31:35 +0800

    Update import mysql.py

--- import mysql.py
+++ import mysql.py
@@ -3,14 +3,19 @@ import mysql.connector
 db = mysql.connector.connect(
     host="localhost",
     user="root",
-    passwd="11111",
-    database="testdb"
+    passwd="11111"
 )
 
 mycursor = db.cursor()
 
+#Create database
+mycursor.execute("CREATE DATABASE mydatabase")
+
+#Show database
 mycursor.execute("SHOW DATABASES")
 
+#Check if Database Exists
 for x in mycursor:
   print(x)
+
 
